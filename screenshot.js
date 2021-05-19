4909090b473d2388f89b53a9088c9e1420c18a10
Jay Tran <jaytran@Jays-MacBook-Air.local>
2021-05-19 05:18:22 -0400

    commit project

---
+++ screenshot.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer");
+
+module.exports = function (url) {
+  return new Promise((resolve, reject) => {
+    (async () => {
+      const browser = await puppeteer.launch({
+        // headless: true, // debug only
+        args: ["--no-sandbox"],
+      });
+
+      const page = await browser.newPage();
+
+      await page.goto(url, {
+        waitUntil: ["load", "networkidle0", "domcontentloaded"],
+      });
+
+      // await page.emulateMedia('screen')
+
+      const buffer = await page.screenshot({
+        fullPage: true,
+        type: "png",
+      });
+
+      await browser.close();
+
+      resolve(buffer);
+    })();
+  });
+};
